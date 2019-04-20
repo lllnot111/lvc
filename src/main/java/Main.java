baf6afeabd5575c6c15a063eac1df4cf36dbd0f5
lance <3287596021@qq.com>
2019-04-20 15:44:13 +0800

    lvc不小心把main函数注释掉了

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,28 +24,28 @@ public class Main {
 //        new Diff().diffFile(now,old);
 //        new Diff().option(args);
 
-//        OptionFactory optionFactory = new OptionFactory();
-//        if(args.length<1){
-//            System.out.println("请输入操作内容！");
-//        }else{
-//            AbstractOption option = optionFactory.option(args[0]);
-//            HashSet<String> set = new HashSet<>();
-//            set.add("提交");
-//            set.add("commit");
-//            set.add("上传");
-//            set.add("push");
-//            set.add("回滚");
-//            set.add("reset");
-//            if(set.contains(args[0])){
-//               // new Pull().pull();
-//            }
-//            if(option!=null){
-//                option.option(args);
-//            }else{
-//                System.out.println("不存在的操作！");
-//            }
-//        }
-        new BranchUtil().countLine(new File("F:\\mygit\\lvc\\src"),0);
+        OptionFactory optionFactory = new OptionFactory();
+        if(args.length<1){
+            System.out.println("请输入操作内容！");
+        }else{
+            AbstractOption option = optionFactory.option(args[0]);
+            HashSet<String> set = new HashSet<>();
+            set.add("提交");
+            set.add("commit");
+            set.add("上传");
+            set.add("push");
+            set.add("回滚");
+            set.add("reset");
+            if(set.contains(args[0])){
+               // new Pull().pull();
+            }
+            if(option!=null){
+                option.option(args);
+            }else{
+                System.out.println("不存在的操作！");
+            }
+        }
+        //new BranchUtil().countLine(new File("F:\\mygit\\lvc\\src"),0);
     }
 
 
